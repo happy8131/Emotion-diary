204956a4a754ab6e1397caea4e13c85e58ac2932
happy8131 <lion8131@naver.com>
2022-06-01 00:00:17 +0900

    [Add] react router1

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import RouteTest from "./components/RouteTest";
+import Diary from "./pages/Diary";
+import Edit from "./pages/Edit";
+import Home from "./pages/Home";
+import New from "./pages/New";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h2>App.js</h2>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/diary" element={<Diary />} />
+        </Routes>
+        <RouteTest />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
