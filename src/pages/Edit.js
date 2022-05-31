912a7d622daab121a69c3251dc6be23790609089
happy8131 <lion8131@naver.com>
2022-06-01 00:30:45 +0900

    [Add] React-Hook 7.3

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,37 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  console.log("id: ", id);
+
+  const mode = searchParams.get("mode");
+  console.log("mode :", mode);
+
   return (
     <div>
       <h1>Edit</h1>
       <p>이곳은 수정페이지 입니다.</p>
+      <button onClick={() => setSearchParams({ who: "winterlood" })}>
+        QS 바꾸기
+      </button>
+
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        home
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로가기
+      </button>
     </div>
   );
 };
